8633d3233ab0baa049aaea68b2bffae5df69001d
InstantRamenPack <ray2009zhu@gmail.com>
2026-01-13 15:05:47 -0800

    Just use pymysql generally

--- db.py
+++ db.py
@@ -1,38 +1,34 @@
-try:
-    from flask_mysqldb import MySQL
-except ImportError:
-    # ChatGPT; for Vercel
-    import pymysql
-    import os
-    from flask import current_app, g
+import pymysql
+import os
+from flask import current_app, g
 
-    class MySQL:
-        def __init__(self, app=None):
-            if app is not None:
-                self.init_app(app)
+class MySQL:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
 
-        def init_app(self, app):
-            app.teardown_appcontext(self.teardown)
+    def init_app(self, app):
+        app.teardown_appcontext(self.teardown)
 
-        @property
-        def connection(self):
-            if 'db_conn' not in g:
-                g.db_conn = pymysql.connect(
-                    host=current_app.config["MYSQL_HOST"],
-                    user=current_app.config["MYSQL_USER"],
-                    password=current_app.config["MYSQL_PASSWORD"],
-                    database=current_app.config["MYSQL_DB"],
-                    port=int(current_app.config["MYSQL_PORT"]),
-                    cursorclass=pymysql.cursors.DictCursor,
-                    ssl={"ca": os.path.join(os.path.dirname(__file__), "tidb-ca.pem")},
-                    autocommit=True,
-                )
-            return g.db_conn
+    @property
+    def connection(self):
+        if 'db_conn' not in g:
+            g.db_conn = pymysql.connect(
+                host = current_app.config["MYSQL_HOST"],
+                user = current_app.config["MYSQL_USER"],
+                password = current_app.config["MYSQL_PASSWORD"],
+                database = current_app.config["MYSQL_DB"],
+                port = int(current_app.config["MYSQL_PORT"]),
+                cursorclass = pymysql.cursors.DictCursor,
+                ssl = {"ca": os.path.join(os.path.dirname(__file__), "tidb-ca.pem")},
+                autocommit = True,
+            )
+        return g.db_conn
 
-        def teardown(self, exception):
-            db = g.pop('db_conn', None)
-            if db is not None:
-                db.close()
+    def teardown(self, exception):
+        db = g.pop('db_conn', None)
+        if db is not None:
+            db.close()
 
 mysql = MySQL()
 
