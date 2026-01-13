24de190a0e63c9884a37cfe62f8229a87915e17b
InstantRamenPack <ray2009zhu@gmail.com>
2026-01-13 14:44:15 -0800

    fix again

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ MYSQL_HOST = environ.get('MYSQL_HOST')
 MYSQL_USER = environ.get('MYSQL_USER')
 MYSQL_PASSWORD = environ.get('MYSQL_PASSWORD')
 MYSQL_DB = environ.get('MYSQL_DB')
+MYSQL_PORT = environ.get('MYSQL_PORT')
 MYSQL_CURSORCLASS = 'DictCursor'
 
 GOOGLE_CLIENT_ID = environ.get('GOOGLE_CLIENT_ID')
